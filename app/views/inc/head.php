a284f142c5a40c8fc2eb5c282c42133ce99e21c8
Aly-yoryo <jpalmahrz@gmail.com>
2026-02-01 22:42:29 -0600

    funcionando

--- app/views/inc/head.php
+++ app/views/inc/head.php
@@ -1,28 +1,27 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php echo APP_NAME; ?></title>
+<title><?=APP_NAME; ?></title>
 
 <!-- ************************ CSS Files ************************************ -->
-    <link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/css/animate.css" />
-    <link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/fonts/bootstrap/bootstrap-icons.css" />
-    <link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/css/main.min.css?v1.1" />
+    <link rel="stylesheet" href="<?=APP_URL; ?>app/views/css/animate.css" />
+    <link rel="stylesheet" href="<?=APP_URL; ?>app/views/fonts/bootstrap/bootstrap-icons.css" />
+    <link rel="stylesheet" href="<?=APP_URL; ?>app/views/css/main.min.css?v1.1" />
+    <link rel="stylesheet" href="<?=APP_URL; ?>app/views/css/estilos.css" />
 
     <!-- ************************* Vendor Css Files ************************* -->
 
     <!-- Scrollbar CSS -->
-    <link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/vendor/overlay-scroll/OverlayScrollbars.min.css" />
+    <link rel="stylesheet" href="<?=APP_URL; ?>app/views/vendor/overlay-scroll/OverlayScrollbars.min.css" />
     
-<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/css/sweetalert2.min.css">
-<script src="<?php echo APP_URL; ?>app/views/js/sweetalert2.all.min.js" ></script>
+<link rel="stylesheet" href="<?=APP_URL; ?>app/views/css/sweetalert2.min.css">
+<script src="<?=APP_URL; ?>app/views/js/sweetalert2.all.min.js" ></script>
     <link href="https://kendo.cdn.telerik.com/themes/12.3.0/default/default-purple.css" rel="stylesheet" />
 
 
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <!-- kendo ui -->
-    <script src="<?php echo APP_URL; ?>app/views/js/kendo_v2.js"></script>
+    <script src="<?=APP_URL; ?>app/views/js/kendo_v2.js"></script>
 
 
-    <link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/css/kendo1.css">
-    <link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/css/kendo2.css">
-             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
